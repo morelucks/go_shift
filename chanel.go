b2aa47aef9a8aaf1d27a6b8c9f35eec2c0be3714
morelucks <luckykamshak@gmail.com>
2025-02-03 21:21:33 +0100

    modification of chanel.go

--- chanel.go
+++ chanel.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+	// "crypto/rand"
 	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 	// "go/format"
 )
+
+func DoWork() int {
+	time.Sleep(time.Second)
+	return rand.Intn(100)
+}
 
 func main()  {
 	dataChan:=make(chan int)
 	go func ()  {
-		// background thread
-		for i:=0; i<100; i ++ {
-			dataChan <-i
+		// background threada
+		wg:= sync.WaitGroup{}
+		for i:=0; i<=100; i ++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				result := DoWork()
+				dataChan <-result
+			}()
+			
 		}
+		wg.Wait()
+		close(dataChan) // i.e after sending data to the channel you have to close it, if not it will result to deadlock
 	}()
 		//  main thread
 	for n:= range dataChan{
